66468384a3dc246d0ac8741f340034276136b470
Roberto Javier Lagger <javierlaggerr@gmail.com>
2024-08-30 23:27:49 -0300

    a

--- templatesDemo/templatesApp/urls.py
+++ templatesDemo/templatesApp/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+from templatesApp.views import renderTemplate
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('templates/', include('templatesApp.urls')),
+    path('template/', renderTemplate),
 ]
--- templatesDemo/templatesApp/views.py
+++ templatesDemo/templatesApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def renderTemplate(request):
+    dicionario = {'nome': 'Javier', 'idade': 27}
+    return render(request, 'templatesApp/firstTemplate.html', context=dicionario)
