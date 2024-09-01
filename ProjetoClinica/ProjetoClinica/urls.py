3dd0b77bb4167cd733f684c2e30f73a322440f52
Roberto Javier Lagger <javierlaggerr@gmail.com>
2024-09-01 09:05:47 -0300

    x

--- ProjetoClinica/ProjetoClinica/urls.py
+++ ProjetoClinica/ProjetoClinica/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for formsDemo project.
+URL configuration for ProjetoClinica project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('formsApp.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- formsDemo/formsApp/views.py
+++ formsDemo/formsApp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from formsApp.models import Employee
 from . import forms
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def userRegistrationView(request):
     form=forms.UserRegistrationForm()
     if request.method=='POST':
@@ -11,15 +13,16 @@ def userRegistrationView(request):
             form.save()
     return render(request, 'formsDemo/userRegistration.html', {'form':form})
 
+@login_required
 def principalView(request):
     return render(request, 'formsDemo/principal.html')
 
-
+@login_required
 def consultaView(request):
     context={'empregados': Employee.objects.all()}
     return render(request, 'formsDemo/consultar.html', context)
 
-
+@login_required
 def adicionarView(request):
     context = {'form': forms.EmpregadoForm()}
     if request.method=='POST':
